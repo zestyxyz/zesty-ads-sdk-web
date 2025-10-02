ed972304e2072f9fd3f0e5714ec63a81864b6681
elijah <laughinglemur@protonmail.com>
2025-10-02 15:09:38 -0400

    [unity] Update ads server API endpoint

--- unity/Assets/ZestySDK/Scripts/Internal/Utils/Constants.cs
+++ unity/Assets/ZestySDK/Scripts/Internal/Utils/Constants.cs
@@ -4,7 +4,7 @@
         public const string ZESTY_URL = "https://relay.zesty.xyz";
         public const string BEACON_URL = "https://beacon.zesty.market/api/v1";
         public const string BEACON2_URL = "https://beacon2.zesty.market/zgraphql";
-        public const string AD_SERVER_URL = "https://api.zesty.xyz/api";
+        public const string AD_SERVER_URL = "https://api.zesty.market/api";
         public const string CDN_URL = "https://cdn.zesty.xyz";
         public const bool PREBID = true;
         public const int MAX_PREBID_RETRIES = 4;
