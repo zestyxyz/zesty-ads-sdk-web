77de176f2f7f8b06b540c33e16a30062c7b2a642
limbofeather <elijah@borellion.com>
2026-05-15 12:52:52 -0400

    Remove beacon build and add wle

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,6 @@ cd aframe && yarn && yarn run build
 cd ..
 cd babylonjs && yarn && yarn run build
 cd ..
-cd beacon && yarn && yarn run build
-cd ..
 cd playcanvas && yarn && yarn run build:editor && yarn run build:no-editor
 cd ..
 cd r3f && yarn && yarn run build
@@ -16,16 +14,18 @@ cd threejs && yarn && yarn run build
 cd ..
 cd web && yarn && yarn run build
 cd ..
+cd wonderland && yarn && yarn run build
+cd ..
 cd utils && yarn && yarn run build && yarn run build-networking
 cd ..
 
 cp -R aframe/dist dist
 cp -R babylonjs/dist/* dist
-cp -R beacon/dist* dist
 cp -R playcanvas/dist/* dist
 cp -R r3f/dist/* dist
 cp -R threejs/dist/* dist
-cp unity/zesty-unity-sdk.unitypackage dist
+cp -R wonderland/dist/* dist
+cp unity/borellion-unity-sdk.unitypackage dist
 cp -R web/dist/* dist
 cp -R utils/dist/* dist
 cp -R assets dist
