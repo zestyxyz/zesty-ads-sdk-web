e307c78924fd9dbd10d1ee25d57bbb54f859b1a8
limbofeather <elijah@borellion.com>
2026-05-15 12:35:52 -0400

    Remove beacon, rely on github.com/borellion/beacon instead

--- beacon/src/index.js
+++
@@ -1,8 +0,0 @@
-import { sendOnLoadMetric } from '../../utils/networking';
-
-const spaceId = document.currentScript.getAttribute('data-id');
-if (!spaceId) {
-  console.warn('Borellion Beacon script has been added but no space ID was given!');
-} else {
-  sendOnLoadMetric(spaceId);
-}
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,6 @@ cd aframe && yarn && yarn run build
 cd ..
 cd babylonjs && yarn && yarn run build
 cd ..
-cd beacon && yarn && yarn run build
-cd ..
 cd playcanvas && yarn && yarn run build:editor && yarn run build:no-editor
 cd ..
 cd r3f && yarn && yarn run build
@@ -21,7 +19,6 @@ cd ..
 
 cp -R aframe/dist dist
 cp -R babylonjs/dist/* dist
-cp -R beacon/dist* dist
 cp -R playcanvas/dist/* dist
 cp -R r3f/dist/* dist
 cp -R threejs/dist/* dist
